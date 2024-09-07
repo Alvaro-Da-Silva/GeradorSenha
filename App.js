d5c7e89b2099beb6f7ebc2bc65692d665a46a90c
Alvaro Da Silva <133697238+Alvaro-Da-Silva@users.noreply.github.com>
2024-09-07 18:39:16 -0300

    função para gerar a senha

--- App.js
+++ App.js
@@ -5,11 +5,16 @@ import Slider from '@react-native-community/slider'
 
 export default function App() {
 
-  let senhas ="a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 0 1 2 3 4 5 6 7 8 9"
+  let senhas ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
   const[size,setSize] = useState(10)
+  const[valorsenha,Setvalorsenha] = useState("")
 
   function gerarsenha(){
-    console.log("VASCO")
+    let senha = "";
+    for(let i = 0, n = senhas.length; i < size; i++){
+      senha += senhas.charAt(Math.floor(Math.random() * n))
+    }
+    Setvalorsenha(senha);
   }
 
 
